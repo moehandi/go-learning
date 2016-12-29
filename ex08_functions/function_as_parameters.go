fa2fef46dac1dce947185514d3be61e2d1e1fb6d
moehandi <moehandi@gmail.com>
2016-12-29 10:34:25 +0700

    function as parameter

--- ex08_functions/function_as_parameters.go
+++ ex08_functions/function_as_parameters.go
@@ -1,1 +1,16 @@
-package ex08_functions
+package main
+
+import "fmt"
+
+func main() {
+	callback(2, Add)
+}
+
+func Add(a, b int) {
+	fmt.Printf("the sum of %d and %d is : %d\n", a, b, a + b)
+}
+
+func callback(y int, f func(int, int)) {
+	f(y, 2)
+}
+
