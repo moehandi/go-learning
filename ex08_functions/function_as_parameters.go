e20eaa85051b8605813126a4c4a47688d3afb0f0
moehandi <moehandi@gmail.com>
2016-12-30 11:30:24 +0700

    update function as parameter

--- ex08_functions/function_as_parameters.go
+++ ex08_functions/function_as_parameters.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
 	callback(2, Add)
+
+	// example  case using function as parameter is at pkg
+	// strings.IndexFunc that return index position
+	str := "String 世界"
+	f := func(r rune) bool {
+		return unicode.Is(unicode.Han, r)
+	}
+	fmt.Println(strings.IndexFunc(str, f))
 }
 
 func Add(a, b int) {
