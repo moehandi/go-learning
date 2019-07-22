2046d7b3512019df25dec3c0540eba721558df71
gokyl <gokyland@gmail.com>
2019-07-22 17:10:51 +0700

    rearange import

--- web_development/module1_net_http/ex03_HandlerFunc/main.go
+++ web_development/module1_net_http/ex03_HandlerFunc/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-		"net/http"
-		"log"
-		"fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type myHandler  struct {
--- web_development/module1_net_http/ex06_DefaultServeMux/main.go
+++ web_development/module1_net_http/ex06_DefaultServeMux/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-		"net/http"
-		"fmt"
-		"log"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func messageHandleFunc(w http.ResponseWriter, r *http.Request) {
